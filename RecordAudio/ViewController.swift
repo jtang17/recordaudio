5704a0ba507ca6b542b9ea9b455c3b9069e09127
jtang17 <j.tang17@gmail.com>
2015-10-23 18:55:49 +0800

    Initial Commit

---
+++ RecordAudio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordAudio
+//
+//  Created by jonathan tang on 10/23/15.
+//  Copyright © 2015 jonathan tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
