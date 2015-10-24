b088aa3e90ad656c4663260ac910d4af126ae553
jtang17 <j.tang17@gmail.com>
2015-10-24 12:30:26 +0800

    first commit

--- RecordAudio/ViewController.swift
+++ RecordAudio/ViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        stopButton.hidden = true;
+        recordButton.enabled = true;
+    }
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingLabel.hidden = false;
+        stopButton.hidden = false;
+        recordButton.enabled = false;
+    }
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingLabel.hidden = true;
+        recordButton.enabled = true;
+        stopButton.hidden = true;
     }
 
 
